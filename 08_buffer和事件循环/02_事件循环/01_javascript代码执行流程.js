1500af53f6a9d06a2645d2b20a40145c217cb261
luzilong1993 <luzilong@mininglamp.com>
2021-12-01 16:43:21 +0800

    浏览器中事件循环

---
+++ 08_buffer和事件循环/02_事件循环/01_javascript代码执行流程.js
@@ -0,0 +1,14 @@
+const message = "Hello World";
+
+console.log(message);
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function bar() {
+    return sum(20, 30)
+};
+const result = bar();
+
+console.log(result);
