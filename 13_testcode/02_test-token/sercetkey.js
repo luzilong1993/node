c730d0bc9e96fd834a99a22be4a8c1338089029a
luzilong1993 <luzilong@mininglamp.com>
2021-12-22 10:11:12 +0800

    实现非对称加密

--- 13_testcode/02_test-token/sercetkey.js
+++ 13_testcode/02_test-token/sercetkey.js
@@ -1,5 +1,4 @@
 // token演练
-const fs = require('fs');
 
 const Koa = require('koa');
 const Router = require('koa-router');
@@ -9,19 +8,16 @@ const jwt = require('jsonwebtoken');
 const app = new Koa();
 const testRouter = new Router();
 
-// 在一个项目中的任何一个地方的相对路径，都是相对于process.cwd
+const SERCET_KEY = 'SERCET_KEY';
 
-const PRIVATE_KEY = fs.writeFileSync('./keys/private.key');
-const PUBLIC_KEY = fs.writeFileSync('./keys/public.key');
 
 
 
 
 testRouter.get('/test', (ctx, next) => {
     const user = { id: 110, user: 'why' }
-    const token = jwt.sign(user, PRIVATE_KEY, { //过期时间等
-        expiresIn: 100000,
-        algorithm: 'RS256'
+    const token = jwt.sign(user, SERCET_KEY, { //过期时间等
+        expiresIn: 100000
     })
 
     ctx.body = token;
@@ -34,9 +30,7 @@ testRouter.get('/demo', (ctx, next) => {
     // token认证
     console.log(token);
     try {
-        const result = jwt.verify(token, PUBLIC_KEY, {
-            algorithms: ['RS256']
-        });
+        const result = jwt.verify(token, SERCET_KEY);
         ctx.body = result;
     } catch (err) {
         ctx.body = 'token无效'
